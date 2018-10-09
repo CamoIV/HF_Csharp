878fc2ec1bfb496759de4cd25d11668d5a31f2c3
Ace Olson <aolson22@gmail.com>
2018-10-08 22:49:17 -0400

    Added more classes and began coding

---
+++ GoFish/GoFish/Deck.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFish
+{
+    class Deck
+    {
+        private List<Card> cards;
+        private Random random = new Random();
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            for(int suit = 0; suit <= 3; suit++)
+            {
+                for(int value = 1; value <= 13; value++)
+                {
+                    cards.Add(new Card((Suits)suit, (Values)value));
+                }
+            }
+        }
+
+        public Deck(IEnumerable<Card> intialCards)
+        {
+            cards = new List<Card>(intialCards);
+        }
+
+        public int Count { get { return cards.Count; } }
+
+        public void Add(Card cardToAdd)
+        {
+            cards.Add(cardToAdd);
+        }
+
+        public Card Deal(int index)
+        {
+            Card cardToDeal = cards[index];
+            cards.RemoveAt(index);
+            return cardToDeal;
+        }
+
+        public Card Deal()
+        {
+            return Deal(0);
+        }
+
+        public void Shuffle()
+        {
+            List<Card> newCards = new List<Card>();
+
+            while(cards.Count > 0)
+            {
+                int cardToMove = random.Next(cards.Count);
+                newCards.Add(cards[cardToMove]);
+                cards.RemoveAt(cardToMove);
+            }
+            cards = newCards;
+        }
+
+        public IEnumerable<string> GetCardNames()
+        {
+            List<string> cardNames = new List<string>();
+            foreach(Card card in cards)
+            {
+                cardNames.Add(card.Name);
+            }
+
+            return cardNames;
+        }
+        
+        public Card Peek(int cardNumber)
+        {
+            return cards[cardNumber];
+        }
+        public bool ContainsValue(Values value)
+        {
+            foreach(Card card in cards)
+            {
+                if(card.Value == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Deck PullOutValues(Values value)
+        {
+            Deck deckToReturn = new Deck(new Card[] { });
+            for(int i = cards.Count - 1; i >= 0; i--)
+            {
+                if(cards[i].Value == value)
+                {
+                    deckToReturn.Add(Deal(i));
+                }
+            }
+            return deckToReturn;
+        }
+        public void Sort()
+        {
+            cards.Sort(new CardComparer_byValue());
+        }
+    }
+}
---
+++ GoFish/GoFish/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoFish
+{
+    class Player
+    {
+        private string name;
+        public string Name { get { return name; } }
+        private Random random;
+        private Deck cards;
+        private TextBox textBoxOnForm;
+
+        public Player(String name, Random random, TextBox textBoxOnForm)
+        {
+            this.name = name;
+            this.random = random;
+            this.textBoxOnForm = textBoxOnForm;
+            this.cards = new Deck(new Card[] { });
+
+            textBoxOnForm.Text = Name + " has just joined the game " + Environment.NewLine;
+        }
+
+        public IEnumerable<Values> PullOutBooks()
+        {
+            List<Values> books = new List<Values>();
+            for (int i = 1; i <= 13; i++)
+            {
+                Values value = (Values)i;
+                int howMany = 0;
+                for (int card = 0; card < cards.Count; card++)
+                    if (cards.Peek(card).Value == value)
+                        howMany++;
+                if (howMany == 4)
+                {
+                    books.Add(value);
+                    cards.PullOutValues(value);
+                }
+            }
+            return books;
+        }
+
+        public Values GetRandomValue()
+        {
+            Card randomCard = cards.Peek(random.Next(cards.Count));
+            return randomCard.Value;
+        }
+    }
+}
