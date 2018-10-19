dd4ec6e7400283b1a11bd892449fef6ff839aecd
Ace Olson <aolson22@gmail.com>
2018-10-19 06:48:06 -0400

    Finished Serialization of Deck of cards

---
+++ SerializeDeckofCards/SerializeDeckofCards/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDeckofCards
+{
+    [Serializable]
+    class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+
+        public static string Plural(Values value)
+        {
+            if (value == Values.Six)
+                return "Sixes";
+            else
+                return value.ToString() + "s";
+        }
+
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return Value.ToString() + " of " + Suit.ToString();
+            }
+        }
+    }
+}
--- SerializeDeckofCards/SerializeDeckofCards/Form1.cs
+++ SerializeDeckofCards/SerializeDeckofCards/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,81 @@ namespace SerializeDeckofCards
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        Random random = new Random();
+
+        private Deck randomDeck(int number)
+        {
+            Deck myDeck = new Deck(new Card[] { });
+
+            for(int i = 0; i < number; i++)
+            {
+                myDeck.Add(new Card((Suits)random.Next(1, 4), (Values)random.Next(1, 14)));
+            }
+            return myDeck;
+        }
+        private void dealCards(Deck deckToDeal, string title)
+        {
+            Console.WriteLine(title);
+
+            while(deckToDeal.Count > 0)
+            {
+                Card nextCard = deckToDeal.Deal(0);
+                Console.WriteLine(nextCard.Name);
+            }
+            Console.WriteLine("-------------------");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Deck deckToWrite = randomDeck(5);
+
+            using (Stream output = File.Create("Deck.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(output, deckToWrite);
+            }
+            dealCards(deckToWrite, "What I just wrote to the file");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using(Stream input = File.OpenRead("Deck.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                Deck deckFromFile = (Deck)bf.Deserialize(input);
+                dealCards(deckFromFile, "What I read from the file");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using(Stream output = File.Create("Deck2.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                for(int i = 1; i <= 5; i++)
+                {
+                    Deck deckToWrite = randomDeck(random.Next(1, 10));
+                    bf.Serialize(output, deckToWrite);
+                    dealCards(deckToWrite, "Deck # " + i + " written");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using(Stream input = File.OpenRead("Deck2.dat"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+
+                for(int i = 1; i <= 5; i++)
+                {
+                    Deck deckToRead = (Deck)bf.Deserialize(input);
+                    dealCards(deckToRead, "Deck # " + i + " read");
+                }
+            }
         }
     }
 }
