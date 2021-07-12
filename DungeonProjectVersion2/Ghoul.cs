38f585c89503c18aaee6f14fef13f42f24bb0248
CamoIV <aolson22@gmail.com>
2021-07-12 19:49:28 -0400

    Created Enemy classes

---
+++ DungeonProjectVersion2/Ghoul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonProjectVersion2
+{
+    class Ghoul:Enemy
+    {
+        public Ghoul(Game game, Point location)
+            : base(game, location, 10)
+        { }
+
+        public override void Move(Random random)
+        {
+            // Your code will go here
+        }
+    }
+}
